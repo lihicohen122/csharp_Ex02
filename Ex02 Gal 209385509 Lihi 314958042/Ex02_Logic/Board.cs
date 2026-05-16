b55fc063278cc0fd54304dcdf642be25dadafb6e
lihicohen122 <lihicohen122@gmail.com>
2026-05-16 12:31:17 +0300

    Created dependency between logic and UI (UI knows logic only) and started working on pklayer and board.

--- Ex02 Gal 209385509 Lihi 314958042/Ex02_Logic/Board.cs
+++ Ex02 Gal 209385509 Lihi 314958042/Ex02_Logic/Board.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ex02_Logic.Enums;
 
 namespace Ex02_Logic
 {
     internal class Board
     {
+        private readonly eCellSign[][] Matrix;
+
+
     }
 }
--- Ex02 Gal 209385509 Lihi 314958042/Ex02_Logic/Player.cs
+++ Ex02 Gal 209385509 Lihi 314958042/Ex02_Logic/Player.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ex02_Logic.Enums;
 
 namespace Ex02_Logic
 {
     internal class Player
     {
+        private int Score { get; }
+        private eCellSign Sign { get;}
+        private bool IsComputer { get;}
+        private string Team { get;}
+
+        public Player(eCellSign i_Sign, bool i_IsComputer, string i_Team)
+        {
+            Sign = i_Sign;
+            IsComputer = i_IsComputer;
+            Team = i_Team;
+            Score = 0;
+        }
     }
 }
