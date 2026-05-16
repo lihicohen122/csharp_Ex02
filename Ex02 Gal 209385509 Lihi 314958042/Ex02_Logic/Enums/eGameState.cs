b10b97f326afd9c6b9ebb6f2b720d75604e972b7
lihicohen122 <lihicohen122@gmail.com>
2026-05-16 12:10:48 +0300

    Added all the relevant classes according to our plan

---
+++ Ex02 Gal 209385509 Lihi 314958042/Ex02_Logic/Enums/eGameState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02_Logic.Enums
+{
+    internal enum eGameState
+    {
+        NotInitialized, Playing, Player1Won, Player2Won, Tie, Quit
+    }
+}
