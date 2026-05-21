e8b511fda18d533af51f3e273b667885ee34b545
lihicohen122 <lihicohen122@gmail.com>
2026-05-21 11:46:11 +0300

    in Ex02_UI: - changed class name to ConsoleUI. - moved left padding calculations to method in order to avoid code duplication.

--- Ex02 Gal 209385509 Lihi 314958042/Ex02_UI/Program.cs
+++ Ex02 Gal 209385509 Lihi 314958042/Ex02_UI/Program.cs
@@ -7,7 +7,7 @@
             StartupMenu menu = new StartupMenu();
             if(menu.TryGetGameConfiguration(out int boardSize, out bool isVsComputer))
             {
-                new ConsoleUserInterface(menu.GetQuitSymbol(), boardSize, isVsComputer).RunGame();
+                new ConsoleUI(menu.GetQuitSymbol(), boardSize, isVsComputer).RunGame();
             }
         }
     }
